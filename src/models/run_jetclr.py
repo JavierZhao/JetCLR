946a77a5258d76e14ecda4e76f50ba6c86e44364
JavierZhao <zhaoziha@usc.edu>
2024-04-10 16:50:09 -0700

    Add total CUDA memory information to print_cuda_memory function

--- src/models/run_jetclr.py
+++ src/models/run_jetclr.py
@@ -191,11 +191,12 @@ def plot_avg_cosine_similarities(args, avg_similarities):
 
 
 def print_cuda_memory(logfile):
+    t = torch.cuda.get_device_properties(0).total_memory
     r = torch.cuda.memory_reserved(0)
     a = torch.cuda.memory_allocated(0)
     f = r - a  # free inside reserved
     print(
-        f"CUDA memory: reserved {r/ np.power(1024,3)}G, allocated {a/ np.power(1024,3)}G, free {f/ np.power(1024,3)}G",
+        f"CUDA memory: total {t / np.power(1024,3)}G, reserved {r/ np.power(1024,3)}G, allocated {a/ np.power(1024,3)}G, free {f/ np.power(1024,3)}G",
         flush=True,
         file=logfile,
     )
